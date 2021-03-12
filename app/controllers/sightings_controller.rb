02df596c2809bb22dacb420f4b10918109ed8f1f
Mari-8 <ryanaltimari8@gmail.com>
2021-03-12 16:09:02 -0600

    commit tree b390ab9e050e3fcb50fa9977e6dfd29a93ed9409

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,13 @@
 class SightingsController < ApplicationController
+
+    def index 
+        def index
+            sightings = Sighting.all
+            render json: SightingSerializer.new(sightings).to_serialized_json
+          end
+    end 
     def show
         @sighting = Sighting.find(params[:id])
-        render json: @sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
+        render json: SightingSerializer.new(sighting).to_serialized_json
     end
 end
